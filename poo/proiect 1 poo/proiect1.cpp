95ce553ae0fdba99ac7a37a00b9cb75c5a1b0e83
irinamanea <maneairina84@gmail.com>
2026-03-20 23:04:03 +0200

    am creat clasa construction si am updatat clasa Player

--- poo/proiect 1 poo/proiect1.cpp
+++ poo/proiect 1 poo/proiect1.cpp
@@ -1,5 +1,7 @@
 //
 // Created by Irina Manea on 20.03.2026.
+////
+// Created by Irina Manea on 20.03.2026.
 //
 
 #include <iostream>
@@ -7,19 +9,59 @@
 
 using namespace std;
 
+enum TipResursa {
+    LEMN,
+    CARAMIDA,
+    OAIE,
+    FAN,
+    PIATRA,
+    NIMIC // pt desert
+};
+
 class Player {
     static int noPlayers;
     char* name;
     const int id;
     int points;
     int noCards;
-    int* resurse;
+    TipResursa* resurse;
+    int sateTotal=5;
+    int oraseTotal=4;
+    int drumuriTotal=15;
 public:
     Player (); // constructor default
     Player (char* name, int points, int noCards); // c cu atribute // no cards??????????
     Player (const Player &obj); // copy constructor
     Player& operator=(const Player &obj); //operatorul egal
     ~Player(); //destructor
+    bool poateConstruiSat() {
+        return sateTotal>0;
+    }
+    void aConstruiSat() {
+        if (sateTotal>0) {
+            sateTotal--;
+        }
+    }
+
+    bool poateConstruiOras() {
+        return oraseTotal>0;
+    }
+    void aConstruiOras() {
+        if (oraseTotal>0) {
+            oraseTotal--;
+        }
+    }
+    bool poateConstruiDrum() {
+        return drumuriTotal>0;
+    }
+    void aConstruiDrum() {
+        if (drumuriTotal>0) {
+            drumuriTotal--;
+        }
+    }
+    void afisarePlayer() {
+        cout<<"Jucator: "<<name<<"| Id: "<<id<<"| Sate ramase "<<sateTotal<< "| Orase ramase "<< oraseTotal<< "| Drumuri ramase "<<drumuriTotal<<endl;
+    }
 };
 int Player:: noPlayers=0;
 Player::Player(): id(noPlayers++) {
@@ -33,7 +75,7 @@ Player:: Player (char* name, int points, int noCards): id(noPlayers++) {
     this-> name= new char[strlen(name)+1];
     strcpy(this-> name, name);
     this->points= points;
-    noCards=0;
+    this->noCards= noCards;
     resurse= nullptr;
 }
 Player:: Player(const Player &obj): id(noPlayers++) {
@@ -41,8 +83,8 @@ Player:: Player(const Player &obj): id(noPlayers++) {
     strcpy(this-> name, obj.name);
     this->points= obj.points;
     this->noCards= obj.noCards;
-    if (noCards > 0 ) {
-        this->resurse= new int[obj.noCards];
+    if (noCards > 0 && obj.resurse!=nullptr) {
+        this->resurse= new TipResursa[obj.noCards];
         for (int i=0; i<noCards; i++) {
             this->resurse[i]= obj.resurse[i];
         }
@@ -63,7 +105,7 @@ Player& Player::operator=(const Player &obj) {
     this->noCards= obj.noCards;
     delete [] this-> resurse;
     if (obj.noCards > 0 && obj.resurse!=nullptr ) {
-        this->resurse= new int[obj.noCards];
+        this->resurse= new TipResursa[obj.noCards];
         for (int i=0; i<obj.noCards; i++) {
             this->resurse[i]= obj.resurse[i];
         }
@@ -81,15 +123,62 @@ Player:: ~Player() {
     }
 }
 
-int main (){
-    Player p1("Andrei", 10, 5);
-    Player p2;
-    p2 = p1;
-    cout << "Jucator creat cu succes!";
-    return 0;
+enum TipConstructie {
+    NONE,
+    SAT,
+    ORAS,
+    DRUM
+};
+class Construction {
+    TipConstructie tip;
+    static int noTotal;
+    const int id;
+    Player* proprietar;
+public:
+    Construction();
+    Construction(TipConstructie tip, Player* proprietar);
+    Construction(const Construction &obj);
+    Construction& operator=(const Construction &obj);
+    ~Construction();
+
+    void afisareConstructie() {
+        cout << "Constructie Id: " << id << " | Tip: ";
+        if(tip == SAT) cout << "SAT";
+        else if(tip == ORAS) cout << "ORAS";
+        else if(tip == DRUM) cout << "DRUM";
+        else cout << "NONE";
+    }
+};
+
+int Construction::noTotal=0;
+Construction :: Construction(): id(noTotal++) {
+    this-> proprietar= nullptr;
+    this->tip=NONE;
+}
+Construction:: Construction(TipConstructie tip, Player* proprietar): id(noTotal++) {
+    this-> proprietar= proprietar;
+    this->tip=tip;
+}
+Construction:: Construction(const Construction &obj): id(noTotal++) {
+    this-> proprietar= obj.proprietar;
+    this->tip=obj.tip;
+}
+Construction& Construction::operator=(const Construction &obj) {
+    if (this == &obj) {
+        return *this;
+    }
+    this-> proprietar= obj.proprietar;
+    this->tip=obj.tip;
+    return *this;
 }
 
+Construction::~Construction() {
 
+}
+
+int main() {
+
+}
 
 // //Player
 // Name, id, colour, timestamp
