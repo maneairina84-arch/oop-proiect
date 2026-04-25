180c694a3009499a4ba37a55818063f80b2ed3c3
irinamanea <maneairina84@gmail.com>
2026-04-25 15:19:40 +0300

    am creat clasa Player

---
+++ poo/proiect 2/Player.cpp
@@ -0,0 +1,80 @@
+//
+// Created by Irina Manea on 15.04.2026.
+//
+
+#include "Player.h"
+
+int Player:: noPlayers=0;
+Player:: Player(): id(noPlayers++),
+    name("unknown"),
+    moneyBalance(1500),
+    currentPosition(0),
+    inJail(false),
+    rundeDeStat(0){}
+
+Player:: Player(std::string name, long moneyBalance): id(noPlayers++) {
+    this->name=name;
+    this->moneyBalance=moneyBalance;
+    this->currentPosition=0;
+    this->inJail=false;
+    this->rundeDeStat=0;
+}
+
+Player:: Player(const Player& obj): id(noPlayers++),
+name(obj.name),
+moneyBalance(obj.moneyBalance),
+currentPosition(obj.currentPosition),
+inJail(obj.inJail),
+rundeDeStat(obj.rundeDeStat),
+ownProperties(obj.ownProperties){}
+
+Player& Player::operator= (const Player& obj) {
+    if (this != &obj) {
+        this-> name= obj.name;
+        this-> moneyBalance= obj.moneyBalance;
+        this-> currentPosition= obj.currentPosition;
+        this-> inJail= obj.inJail;
+        this-> rundeDeStat= obj.rundeDeStat;
+        this-> ownProperties= obj.ownProperties;
+    }
+    return *this;
+}
+
+Player:: ~Player(){}
+
+//getters
+int Player:: getId() const {
+    return id;
+}
+std::string Player::getName() const {
+    return name;
+}
+long Player::getMoneyBalance() const {
+    return moneyBalance;
+}
+int Player::getCurrentPosition() const {
+    return currentPosition;
+}
+bool Player::getInJail() const {
+    return inJail;
+}
+
+std::vector<int> Player::getOwnProperties() const {
+    return ownProperties;
+}
+
+//setters
+void Player:: setName(std::string name) {
+    this->name=name;
+}
+void Player:: setMoneyBalance(long moneyBalance) {
+    this->moneyBalance=moneyBalance;
+}
+void Player:: setCurrentPosition(int currentPosition) {
+    this->currentPosition=currentPosition;
+}
+void Player:: setInJail(bool inJail) {
+    this->inJail=inJail;
+}
+
+//serializare
---
+++ poo/proiect 2/Player.h
@@ -0,0 +1,52 @@
+//
+// Created by Irina Manea on 15.04.2026.
+//
+
+#ifndef UNTITLED_PLAYER_H
+#define UNTITLED_PLAYER_H
+#include<iostream>
+#include<string>
+#include<vector>
+
+
+class Player {
+    static int noPlayers;
+    const int id;
+    std::string name;
+    long moneyBalance;
+    int currentPosition;
+    bool inJail;
+    int rundeDeStat;
+    std::vector<int>ownProperties; //se stocheaza casutacde pe tabla de joc
+
+public:
+    Player();
+    Player(std::string name, long moneyBalance);
+    Player(const Player& obj);
+    Player& operator=(const Player& obj);
+    ~Player();
+
+ //getters
+    int getId() const;
+    std::string getName() const;
+    long getMoneyBalance() const;
+    int getCurrentPosition() const;
+    bool getInJail() const;
+    std::vector<int> getOwnProperties() const;
+
+ //setters
+    void setName(std::string name);
+    void setMoneyBalance(long moneyBalance);
+    void setCurrentPosition(int currentPosition);
+    void setInJail(bool inJail);
+
+
+    friend std:: ostream& operator<< (std:: ostream& os, const Player& obj);
+    friend std:: iostream& operator>>(std:: iostream& is, Player& obj);
+
+
+
+};
+
+
+#endif //UNTITLED_PLAYER_H
